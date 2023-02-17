9fe04c8e49d19ec716d520088211e00af622ea64
Sad <cougar.l@hotmail.com>
2023-02-17 23:19:49 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,32 @@ Hooks.on('renderDialog', () => {
     try {
         const users = game.users.filter(user => user.active);
         let playerNames = users.map(u => u.name)
+        sortLabel('.form-fields', playerNames)
+        sortLabel('.show-to-players', playerNames)
 
-        const formFields = document.querySelector('.form-fields');
-        const playerLabels = formFields.querySelectorAll('label.checkbox');
-        const playerMap = new Map();
-        // 將符合條件的玩家名稱及對應的標籤放入map中
-        playerLabels.forEach(label => {
-            console.log('label', label)
-            const playerName = label.textContent.trim();
-            if (playerNames.includes(playerName)) {
-                playerMap.set(playerName, label);
-            }
-        });
-        // 將map中的標籤依序放在前面
-        playerNames.forEach(playerName => {
-            const label = playerMap.get(playerName);
-            if (label) {
-                formFields.insertBefore(label, formFields.firstChild);
-            }
-        });
 
     } catch (error) {
         console.log('error', error)
     }
 });
+
+function sortLabel(className, playerNames) {
+    const formFields = document.querySelector(className);
+    const playerLabels = formFields.querySelectorAll('label.checkbox');
+    const playerMap = new Map();
+    // 將符合條件的玩家名稱及對應的標籤放入map中
+    playerLabels.forEach(label => {
+        console.log('label', label)
+        const playerName = label.textContent.trim();
+        if (playerNames.includes(playerName)) {
+            playerMap.set(playerName, label);
+        }
+    });
+    // 將map中的標籤依序放在前面
+    playerNames.forEach(playerName => {
+        const label = playerMap.get(playerName);
+        if (label) {
+            formFields.insertBefore(label, formFields.firstChild);
+        }
+    });
+}
